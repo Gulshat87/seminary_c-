8ee90a92461b1cccd2fe5e1088cf4f71cdcde90d
Gulshat <b_gul87@bk.ru>
2022-09-01 22:25:26 +0500

    seminar_9_63

---
+++ seminar_9/zadacha63/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+string Number(int a, int b)
+{
+    if (a <= b) 
+    {
+        return $"{a} " + Number(a + 1,b);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+Console.WriteLine(Number(1,10));
+
